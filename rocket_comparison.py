934e47d3014ad448e7e146ca78853206388332c9
Johnathan Nicolosi <johnathan@nicolosi.space>
2018-10-22 15:16:03 -0600

    Update rocket_comparison.py
    Converted each rocket into a class object

--- rocket_comparison.py
+++ rocket_comparison.py
@@ -1,52 +1,12 @@
-# Rocket Comparison
-#  Written by Johnathan Nicolosi
-#
+########################################################
+#                                                      #
+#                  Rocket Comparison                   #
+#            Written by Johnathan Nicolosi             #
+#                                                      #
+########################################################
+
 import matplotlib.pyplot as plt
 
-################################
-#           Saturn V           #
-################################
-saturn_h = 111 # meters
-saturn_d = 10 # meters
-saturn_t = 7891000 # lbf
-saturn_stages = 3
-saturn_mass = 2840622 #kg
-
-################################
-#           Atlas V            #
-################################
-atlas_h = 62.5 # meters
-atlas_d = 3.81 # meters
-atlas_t = 22300 # lbf
-atlas_stages = 2
-atlas_mass = 21173 #kg
-
-################################
-#           Detla II           #
-################################
-delta_h = 37.8 # meters
-delta_d = 2.4 # meters
-delta_t = 107000 # lbf
-delta_stages = 2 # or 3
-delta_mass = 231870 # kg
-
-################################
-#        Black Brant IV        #
-################################
-brant_h = 5.3 # meters
-brant_d = 0.44 # meters
-brant_t = 25000 # lbf
-brant_stages = 2
-brant_mass = 1265 # kg
-
-################################
-#           Falcon 9           #
-################################
-falcon_h = 70 # meters
-falcon_d = 3.7 # meters
-falcon_t = 1710000 # lbf
-falcon_stages = 2
-falcon_mass = 549054 # kg
 
 ################################
 #       Rocket Comparison      #
@@ -57,7 +17,117 @@ height = [111, 62.5, 37.8, 5.3, 70]
 diameter = [10, 3.81, 2.4, 0.44, 3.7]
 stages = [3, 2, 2, 2, 2]
 mass = [2840622, 21173, 231870, 1265, 549054]
+attributes = ['Height (h):', 'Diameter (d):', 'Thrust (t):', 'Stages (s):', 'Mass (m):']
 
+
+################################
+#           Saturn V           #
+################################
+def saturn_v():
+    h = 111  # meters
+    d = 10  # meters
+    t = 7891000  # lbf
+    s = 3  # stages
+    m = 2840622  # kg
+    rocket = [h, d, t, s, m]
+    print(rockets[0])
+    print(attributes[0], rocket[0])
+    print(attributes[1], rocket[1])
+    print(attributes[2], rocket[2])
+    print(attributes[3], rocket[3])
+    print(attributes[4], rocket[4])
+    print("")
+
+
+saturn_v()
+
+
+################################
+#           Atlas V            #
+################################
+def atlas_v():
+    h = 62.5  # meters
+    d = 3.81  # meters
+    t = 22300  # lbf
+    s = 2
+    m = 21173  # kg
+    rocket = [h, d, t, s, m]
+    print(rockets[1])
+    print(attributes[0], rocket[0])
+    print(attributes[1], rocket[1])
+    print(attributes[2], rocket[2])
+    print(attributes[3], rocket[3])
+    print(attributes[4], rocket[4])
+    print("")
+
+
+atlas_v()
+
+
+################################
+#           Detla II           #
+################################
+def delta_ii():
+    h = 37.8  # meters
+    d = 2.4  # meters
+    t = 107000  # lbf
+    s = 2  # or 3
+    m = 231870  # kg
+    rocket = [h, d, t, s, m]
+    print(rockets[0])
+    print(attributes[0], rocket[0])
+    print(attributes[1], rocket[1])
+    print(attributes[2], rocket[2])
+    print(attributes[3], rocket[3])
+    print(attributes[4], rocket[4])
+    print("")
+
+
+delta_ii()
+
+
+################################
+#        Black Brant IV        #
+################################
+def black_brant_iv():
+    h = 5.3  # meters
+    d = 0.44  # meters
+    t = 25000  # lbf
+    s = 2
+    m = 1265  # kg
+    rocket = [h, d, t, s, m]
+    print(rockets[0])
+    print(attributes[0], rocket[0])
+    print(attributes[1], rocket[1])
+    print(attributes[2], rocket[2])
+    print(attributes[3], rocket[3])
+    print(attributes[4], rocket[4])
+    print("")
+
+
+black_brant_iv()
+
+
+################################
+#           Falcon 9           #
+################################
+def falcon_9():
+    h = 70  # meters
+    d = 3.7  # meters
+    t = 1710000  # lbf
+    s = 2
+    m = 549054  # kg
+    rocket = [h, d, t, s, m]
+    print(rockets[0])
+    print(attributes[0], rocket[0])
+    print(attributes[1], rocket[1])
+    print(attributes[2], rocket[2])
+    print(attributes[3], rocket[3])
+    print(attributes[4], rocket[4])
+    print("")
+
+
+falcon_9()
 
 #################################
 # Plotting Figures (Matplotlib) #
@@ -78,7 +148,6 @@ plt.subplot(211)
 plt.title('Mass Vs Thrust')
 plt.bar(rockets, mass)
 plt.ylabel('Mass (kg)')
-#plt.bar(rockets, stages)
 
 plt.subplot(212)
 plt.bar(rockets, thrust)
